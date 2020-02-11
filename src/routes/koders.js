a325097deffe8205c76b982566f2f13620a9616e
Carlos Silva <carlos@leco.mx>
2020-02-10 21:28:18 -0600

    ADD koders endpoints

--- src/routes/koders.js
+++ src/routes/koders.js
@@ -24,4 +24,92 @@ router.get('/', async (request, response) => {
   }
 })
 
+// POST /koders
+// Objetivo: Crear un koder
+
+// obtener datos del koder de request.body
+// pasar la data al caso de uso koder.create
+//
+
+router.post('/', async (request, response) => {
+  try {
+    const newKoder = await koder.create(request.body)
+    response.json({
+      success: true,
+      message: 'Koder created',
+      data: {
+        koder: newKoder
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+router.get('/:id', async (request, response) => {
+  try {
+    const { id } = request.params
+
+    const koderFound = await koder.getById(id)
+
+    response.json({
+      success: true,
+      message: 'koder found',
+      data: {
+        koder: koderFound
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+router.delete('/:id', async (request, response) => {
+  try {
+    const { id } = request.params
+    const deletedKoder = await koder.deleteById(id)
+    response.json({
+      success: true,
+      message: 'Koder deleted',
+      data: {
+        koder: deletedKoder
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+router.patch('/:id', async (request, response) => {
+  try {
+    const { id } = request.params
+    const koderPatched = await koder.updateById(id, request.body)
+    response.json({
+      success: true,
+      message: 'koder updated',
+      data: {
+        koder: koderPatched
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
 module.exports = router
